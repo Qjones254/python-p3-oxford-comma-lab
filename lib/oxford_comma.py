3e33fd8683ba2fdc9b772692b5f8b417bb16af2b
quincy mwangi <mwangiquincy77@gmail.com>
2024-09-12 13:04:10 +0300

    complete oxford comma lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
 def oxford_comma(items):
-    return None
+    
+    if len(items) == 1:
+        return items[0]
+    
+    if len(items) == 2:
+        return ' and '.join(items)
+    
+    # For three or more items
+    return ', '.join(items[:-1]) + ', and ' + items[-1]
+print(oxford_comma(['']))
+    
